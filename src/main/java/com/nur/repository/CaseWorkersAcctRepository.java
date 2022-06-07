741645c41086d7a6ed3af2e055b719939d48de86
Nurzamal Islam <n.zaman98@gmail.com>
2022-06-07 17:53:44 +0530

    updated

--- src/main/java/com/nur/repository/CaseWorkersAcctRepository.java
+++ src/main/java/com/nur/repository/CaseWorkersAcctRepository.java
@@ -5,5 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.nur.entity.CaseWorkersAcctEntity;
 
 public interface CaseWorkersAcctRepository extends JpaRepository<CaseWorkersAcctEntity, Integer> {
+	
+	public CaseWorkersAcctEntity findByEmailAndPwd(String email, String pazzword);
+	
+	public CaseWorkersAcctEntity findByEmail(String email);
 
 }
